326240e003c9379336c219754a1e9685177264f0
Wborg <wilicious@gmail.com>
2022-08-16 03:35:52 -0300

    okk

--- vite-tailwind/src/App.jsx
+++ vite-tailwind/src/App.jsx
@@ -1,45 +1,24 @@
 import api from '../src/services/axios'
-import { useState, useEffect } from 'react'
+import { Card } from './components/Card'
 
 
 
 function App() {
 
-  const [alunos, setAlunos] = useState([])
-  useEffect( ()=>{
-
-   
-      const getAlunos = async () => {
-        await api.get('/alunos')
-        .then((response)=>{
-          console.log(response.data)
-          setAlunos(response.data)
-        })
-        .catch((erro)=>{
-          console.log(erro)
-        })
-        
-        console.log("--------------")
-    }
-    getAlunos()
-
-    },[])
-        
+    
         
     return (
-      <div>
-        <h1 className="border-2 border-b-blue-700 pb-4">Lista de alunos</h1>
-        <ul>
-          {alunos.map((aluno)=>(
-              <li key={aluno.nome}>
-                Nome: {aluno.nome} -- idade: {aluno.idade}
-              </li>
-            ))
-          }
-        </ul>
+
+      <>
+          <Card/>
+          <div>
+                <p>parágrafo</p>    
+          </div>
+      </>
+      
+         
         
         
-      </div>
     )
   }
   
