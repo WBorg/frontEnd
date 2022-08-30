c17ecf3baf4316ad8ec4b538262834d4ac70e717
WILLY BORGES SILVA <willy.bsilva@senacsp.edu.br>
2022-08-29 22:25:36 -0300

    imagem perfil usuario

--- system-mercado/src/page/UsuariosForm/UsuariosFormView.jsx
+++ system-mercado/src/page/UsuariosForm/UsuariosFormView.jsx
@@ -1,188 +1,167 @@
-import React, { useState , useEffect } from "react"
-import api from '../../services/api'
-import { Link , useHistory } from 'react-router-dom'
-import Form from 'react-bootstrap/Form';
-import Alert from 'react-bootstrap/Alert';
-import Button from 'react-bootstrap/Button';
-import { Container } from 'react-bootstrap';
-import {Eye, EyeSlash} from 'phosphor-react';
-import '../../components/Login/styles.css';
-
-
-
-
-
+import React, { useState, useEffect } from "react";
+import api from '../../services/api';
+import { useHistory } from 'react-router-dom';
+import { Form, Button, Container, Alert, FloatingLabel } from 'react-bootstrap';
+import  NavBar  from '../../components/NavBar/NavBar'
 
 const initialValue = {
-  name: '',
-  email: ''
+    name: '',
+    email: '',
 }
 
-export const UsuariosFormView = () =>{
+export const UsuariosFormView = () => {
 
-  
-  
-  
-  const history = useHistory()
-  const [values, setValues] = useState(initialValue)
-  const [status, setStatus] = useState({
-    type: '',
-    mensagem: '',
-    loading: false
-    
-  })
+    const history = useHistory();
 
-  const [image, setImage] = useState("")
-  const [endImage, setEndImage] = useState("")
-
-  const [pass, setPass] = useState('password')
-  
-  /*funcao para alternar visualizacao da senha*/
-  const changeIcon = ()=>{
-    {pass == 'password' ? setPass('text') : setPass('password')}
-  }
-
-
-  useEffect(()=>{
-    const getUsers = async () =>{
-      const headers = {
-        'headers': {
-          'Authorization' : 'Bearer ' +  localStorage.getItem('token'),
-          'Content-Type' : 'application/json'
-        }
-      }
-      await api.get("/users/view-profile/" + localStorage.getItem('user'), headers)
-      .then((response)=>{
-          if(response.data.users){
-            setValues(response.data.users)
-            setEndImage(response.data.endImagem)
-          }else{
-            setStatus({
-              type: 'warning',
-              mensagem: 'Usuário não encontrado'
-            })
-          }
-      }).catch((error)=>{
-        if(error.response){
-          setStatus({
-            type:'error',
-            mensagem: error.response.data.mensagem
-          })
-        }else{
-            setStatus({
-              type:'error',
-              mensagem: 'Erro: tente mais tarde'
-            })
-        }
-      })
-    }
-  
-    
-   getUsers()
-
-  }, [])
-
-  const formSubmit = async e =>{
-    e.preventDefault()
-    setStatus({loading: true})
-
-    const headers = {
-      'headers': {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + localStorage.getItem('token')
-      }
-    }
-
-    
-      await api.post("/users/create", values, headers)
-      .then((response) =>{
-          console.log(response)
-          setStatus({
-            type: 'success',
-            mensagem: response.data.mensagem,
-            loading: false
-          })
-          return history.push("/usuarios")
-      }).catch((err)=>{
-          if(err.response){
-            setStatus({
-              type: 'error',
-              mensagem: err.response.data.mensagem,
-              loading: false
-            })
-        }else{
-          setStatus({
-            type: 'error',
-            mensagem: 'Erro: tente mais tarde',
-            loading: false
-          })
-        }}
-      )
-      
-    
-      await api.put("/users/update", values, headers)
-    .then((response) =>{
-        console.log(response)
-        setStatus({
-          type: 'success',
-          mensagem: response.data.mensagem,
-          loading: false
-        })
-        return history.push("/usuarios")
-    }).catch((err)=>{
-      if(err.response){
-        setStatus({
-          type: 'error',
-          mensagem: err.response.data.mensagem,
-          loading: false
-        })
-      }else{
-        setStatus({
-          type: 'error',
-          mensagem: 'Erro: tente mais tarde',
-          loading: false
-        })
-      }
-
+    const [values, setValues] = useState(initialValue);
+    const [status, setStatus] = useState({
+        type: '',
+        mensagem: '',
+        loading: false
     })
-
-    
-
-  }
-
-    
-  
+    const [image, setImage] = useState("");
+    const [endImage, setEndImage] = useState("");
 
 
+    useEffect(() => {
 
-  return(
-    <div>
+        const getUser = async () => {
+            const headers = {
+                'headers': {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + localStorage.getItem('token')
+                },
+            }
+            console.log(localStorage.getItem('user'));
 
-    <Container>
-      <h1>{acao} Usuário</h1>
-      <Form onSubmit={formSubmit} className="borderForm">
-          {status.type == 'error' ? <Alert size="big" variant="danger"><p>{status.mensagem}</p></Alert> : ""} 
-          {status.type == 'success' ? <Alert variant="success"><p>{status.mensagem}</p> </Alert> : ""}
-          {status.loading ? <p>Validando...</p> : ""}
-          <Form.Group className="mb-3" controlId="formBasicName">
-            <Form.Label>Nome</Form.Label>
-            <Form.Control type="text" name="name" value={values.name} placeholder="digite seu nome" onChange={valorInput} />
-          </Form.Group>
+            await api.get("/users/view-profile/" + localStorage.getItem('user'), headers)
+                .then((response) => {
+                    console.log(response.data.users);
+                    if (response.data.users) {
+                        setValues(response.data.users);
+                        setEndImage(response.data.endImagem);
+                    } else {
+                        setStatus({
+                            type: 'warning',
+                            mensagem: 'Usuário não encontrado!!!',
+                        })
+                    }
+                }).catch((err) => {
+                    if (err.response) {
+                        setStatus({
+                            type: 'error',
+                            mensagem: err.response.data.mensagem
+                        })
+                    } else {
+                        setStatus({
+                            type: 'error',
+                            mensagem: 'Erro: Tente mais tarde!'
+                        })
+                    }
+                })
+        }
+        getUser();
 
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>E-mail:</Form.Label>
-            <Form.Control type="email" name="email" value={values.email} placeholder="Digite seu e-mail" onChange={valorInput} />
-          </Form.Group>
-          
-          {status.loading 
-          ? <Button variant="primary" type="submit" disabled>Enviando... </Button>
-          : <Button variant="primary" type="submit">Enviar </Button> 
-          }
-            
-         
-        </Form>
-      </Container>
+    }, [])
 
-    </div>
-  )
+    const formSubmit = async e => {
+        e.preventDefault();
+        setStatus({ loading: true });
+
+        const headers = {
+            'headers': {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem('token')
+            },
+        }
+            const formData = new FormData()
+            formData.append('image',image)
+
+            await api.put("/users/edit-profile-image", formData, headers)
+                .then((response) => {
+                    setStatus({ loading: false })
+                    return history.push('/usuarios')
+                }).catch((err) => {
+                    if (err.response) {
+                        setStatus({
+                            type: 'error',
+                            mensagem: err.response.data.mensagem,
+                            loading: false
+                        })
+                    } else {
+                        setStatus({
+                            type: 'error',
+                            mensagem: 'Erro: tente mais tarde',
+                            loading: false
+                        })
+                    }
+
+                })
+
+    }
+
+    return (
+        <>
+            <NavBar />
+            <Container className="box">
+                <Form onSubmit={formSubmit} className="borderForm">
+                    <h2>Usuário</h2>
+
+                    {status.type == 'error'
+                        ? <Alert variant="danger">{status.mensagem}</Alert>
+                        : ""}
+                    {status.type == 'success'
+                        ? <Alert variant="success">{status.mensagem}</Alert>
+                        : ""}
+
+                    {status.loading ? <Alert variant="warning">Enviando...</Alert> : ""}
+                    <FloatingLabel
+                        controlId="floatingInput"
+                        label="Name"
+                        className="mb-3"
+                    >
+                        <Form.Control type="text" value={values.name}/>
+                    </FloatingLabel>
+
+                    <FloatingLabel
+                        controlId="floatingInput"
+                        label="E-mail"
+                        className="mb-3"
+                    >
+                        <Form.Control type="text" value={values.email}/>
+                    </FloatingLabel>
+
+                    <Form.Group className="mb-3" controlId="formBasicImage">
+                        <Form.Label>
+                            *Imaggem:
+                        </Form.Label>
+                        <Form.Control type="file" name="image" onChange={(e)=> setImage(e.target.files[0])}/>
+                    </Form.Group>
+                    {image ?
+                        (
+                            <img src={URL.createObjectURL(image)}
+                                alt="imagem do usuário"
+                                width="150"
+                                height="150"
+                             />
+                        ) : (
+                            <img 
+                                src={endImage}
+                                alt="Imagem do usuário"
+                                width="150"
+                                height="150"
+                            />
+
+                        )
+                    }
+
+                    {status.loading
+                        ? <Button variant="primary" disabled type="submit">Enviando...</Button>
+                        : <Button variant="primary" type="submit">Enviar</Button>
+                    }
+
+                </Form>
+            </Container>
+        </>
+    )
 }
